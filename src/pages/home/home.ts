455e6fa341305ac602b1dfd35c8da325a10e5b4b
Patricia Duduzile <patricia.deemasango@gmail.com>
2018-06-29 09:50:18 -0700

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,56 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  checked = false;
+  increment = 0;
+  value;
+  Val;
+  value1;
+  Val1;
+  name;
+  total = 0;
+  message = "Thank you";
+  price = 14;
+  summary:boolean;
+
   constructor(public navCtrl: NavController) {
+    this.summary=false;
 
   }
 
+  minus(){
+  
+    if(this.increment >0){
+    this.increment -=1;
+  }
+  
+  }
+
+  add(){
+
+    this.increment+=1;
+  }
+
+  onOrder(){
+   this.summary=true;
+
+   if(this.value ==true){
+     this.Val = "+Whipped cream";
+   }
+
+   if(this.value1 == true){
+     this.Val1 = "+chocolate"
+   }
+
+   
+
+   this.total=this.price*this.increment;
+
+  }
+
+  onReset(){
+
+    
+    this.summary=false;
+  }
 }
